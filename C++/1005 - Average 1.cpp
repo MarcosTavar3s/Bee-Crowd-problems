d516a88262e3875b9779c035160e43bdc603bfcf
MarcosTavar3s <86982304+MarcosTavar3s@users.noreply.github.com>
2024-01-24 09:35:52 -0300

    Create  1005 - Average 1.cpp

---
+++ C++/1005 - Average 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+    double a,b;
+    cin >> a >> b;
+    double med= ((3.5*a)+(7.5*b))/11;
+
+    cout << "MEDIA = " << fixed << setprecision(5) << med << endl;
+
+
+    return 0;
+}
